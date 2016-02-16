6c9c18f29c7e664871512c8a26d12777b3fad838
Nate Dalo <nate.dalo@yahoo.com>
2016-02-16 17:01:28 -0600

    Add validation errors to configurations

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -1,3 +1,7 @@
 class Configuration < ActiveRecord::Base
   belongs_to :user
+
+  validates :primary_marketplace_id, presence: true
+  validates :merchant_id, presence: true
+  validates :auth_token, presence: true
 end
--- spec/features/create_configuration_spec.rb
+++ spec/features/create_configuration_spec.rb
@@ -1,16 +1,31 @@
 require "rails_helper"
 
 feature "Create configuration" do
-  scenario "successfully" do
+  before do
     user = create(:user)
     sign_in_as!(user)
-
     visit new_configuration_path
+  end
+
+  scenario "successfully" do
     fill_in "Primary Marketplace ID", with: "primarymarketplaceid"
     fill_in "Merchant ID", with: "merchantid"
     fill_in "Authorization Token", with: "authtoken"
     click_on "Create Configuration"
 
     expect(page).to have_content("Configuration was successfully created.")
+  end
+
+  scenario "unsuccessfully" do
+    fill_in "Primary Marketplace ID", with: ""
+    fill_in "Merchant ID", with: ""
+    fill_in "Authorization Token", with: ""
+    click_on "Create Configuration"
+
+    within(".errors") do
+      expect(page).to have_content("Primary Marketplace ID can't be blank")
+      expect(page).to have_content("Merchant ID can't be blank")
+      expect(page).to have_content("Authorization Token can't be blank")
+    end
   end
 end
