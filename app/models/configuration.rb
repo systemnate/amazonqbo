6713ebb3ec2a6838c32aeb9a6bedf7c64616eb28
Nate Dalo <nate.dalo@yahoo.com>
2016-02-17 14:24:17 -0600

    Add length validations to configuration and fix issue with how link is presented in header view

--- app/models/configuration.rb
+++ app/models/configuration.rb
@@ -1,7 +1,7 @@
 class Configuration < ActiveRecord::Base
   belongs_to :user
 
-  validates :primary_marketplace_id, presence: true
-  validates :merchant_id, presence: true
-  validates :auth_token, presence: true
+  validates :primary_marketplace_id, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :merchant_id, presence: true, length: { minimum: 5, maximum: 100 }
+  validates :auth_token, presence: true, length: { minimum: 5, maximum: 100 }
 end
